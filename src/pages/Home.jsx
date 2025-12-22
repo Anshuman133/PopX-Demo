89e620a70a4ed5cfb7a3afd055ac1dbb800cc232
Anshuman133 <paridaanshuman48@gmail.com>
2025-12-22 20:39:07 +0530

    Update bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
      <div className="flex justify-center items-center min-h-screen bg-white">
-      <div className="w-full max-w-md h-screen bg-gray-100 flex flex-col justify-end relative overflow-hidden">
+      <div className=" w-[370px] min-h-[80vh] max-w-md h-screen bg-gray-100 flex flex-col justify-end relative overflow-hidden">
         <div className="mx-5 mb-7 space-y-3">
-        <h1 className="font-bold text-2xl">Welcome to PopX</h1>
+        <h1 className="font-bold text-2xl text-gray-900">Welcome to PopX</h1>
         <p className="text-gray-600 text-base">Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit.</p>
         
         <div className="flex flex-col  space-y-3 font-semibold mt-10">
