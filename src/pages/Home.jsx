b60a5377a1ca905248564c9003b21788de348fce
Anshuman133 <paridaanshuman48@gmail.com>
2025-12-22 20:51:14 +0530

    Update bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
      <div className="flex justify-center items-center min-h-screen bg-white">
       <div className=" w-[370px] min-h-[80vh] max-w-md h-screen bg-gray-100 flex flex-col justify-end relative overflow-hidden">
-        <div className="mx-5 mb-7 space-y-3">
+        <div className="mx-5 mb-7 space-y-2">
         <h1 className="font-bold text-2xl text-gray-900">Welcome to PopX</h1>
-        <p className="text-gray-600 text-base">Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit.</p>
+        <p className="text-gray-500 text-[17px]  ">Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit.</p>
         
-        <div className="flex flex-col  space-y-3 font-semibold mt-10">
+        <div className="flex flex-col  space-y-3 font-semibold mt-5">
         <Link  to="/create-account">
         <button className="bg-[#6c25ff] cursor-pointer w-full hover:bg-violet-500 text-white rounded-md py-3  ">Create Account</button>
         </Link>
